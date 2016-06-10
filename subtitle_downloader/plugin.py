bf00ef06affe50adebbf78f74238e7380f0d1b0d
Dor Munis <dormunis@gmail.com>
2016-06-10 15:30:52 +0300

    added different providers and initiazliations for them

--- subtitle_downloader/plugin.py
+++ subtitle_downloader/plugin.py
@@ -6,37 +6,43 @@ from kamerie.template import TemplatePlugin
 from subtitle_downloader.subtitle_provider import SubtitleProvider
 
 
-class SubtitleDownloader(TemplatePlugin):
-
-    def __init__(self, plugin_name):
-        super(SubtitleDownloader, self).__init__(plugin_name)
+class Plugin(TemplatePlugin):
+    def __init__(self, plugin_name, path):
+        super(Plugin, self).__init__(plugin_name, path)
 
         # provider handling
-        self.providers = self.__dict__.get('settings', [])
+        provider_list = self.__dict__.get('settings', [])
+        self.providers = self.register_providers(provider_list)
 
     def on_message(self, message):
         for provider in self.providers:
-            module = provider.get('module', '')
+            subtitles = provider.get_subtitles(message)
+            self.handle_result(subtitles)
+
+    def register_providers(self, providers):
+        valid_subtitles_providers = []
+
+        for module, provider in providers.iteritems():
+            module = '{module}.py'.format(module=module)
             expected_class = provider.get('class', '')
 
             try:
-                py_mod = imp.load_source(module, os.getcwd())
-                if hasattr(py_mod, expected_class):
+                subtitle_module = imp.load_source(module, os.path.join(self.path, module))
+                if hasattr(subtitle_module, expected_class):
                     settings = deepcopy(provider)
-                    settings.pop('module')
                     settings.pop('class')
-                    class_inst = getattr(py_mod, expected_class)(**settings)
-
-                    if isinstance(class_inst, SubtitleProvider):
-                        subtitles = class_inst.get_subtitles(message)
-                        self.handle_result(subtitles)
+                    subtitle_provider = getattr(subtitle_module, expected_class)(path=self.path, **settings)
+                    if isinstance(subtitle_provider, SubtitleProvider):
+                        valid_subtitles_providers.append(subtitle_provider)
                     else:
-                        self._logger.error('%s is not a vialbe subtitle provider class' % expected_class)
+                        self._logger.error('%s is not a viable subtitle provider class' % expected_class)
                 else:
                     self._logger.error('module {module} does not have a class called: {cls}'.
                                        format(module=module, cls=expected_class))
             except ImportError:
                 self._logger.error('no module found: %s' % module)
+
+        return valid_subtitles_providers
 
     def handle_result(self, subs):
         pass
